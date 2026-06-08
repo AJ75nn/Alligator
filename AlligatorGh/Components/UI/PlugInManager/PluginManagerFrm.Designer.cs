dd2a9f3212ef77e552e338718e4077ae18cd3ee3
Ali Javaheri <aj75nn@yahoo.com>
2026-06-08 08:40:35 +0330

    fix: changed size of the form

--- AlligatorGh/Components/UI/PlugInManager/PluginManagerFrm.Designer.cs
+++ AlligatorGh/Components/UI/PlugInManager/PluginManagerFrm.Designer.cs
@@ -52,9 +52,9 @@ namespace AlligatorGh.Components.UI.PlugInManager
             flpTabList.BorderStyle = BorderStyle.FixedSingle;
             flpTabList.Dock = DockStyle.Fill;
             flpTabList.FlowDirection = FlowDirection.TopDown;
-            flpTabList.Location = new System.Drawing.Point(23, 84);
+            flpTabList.Location = new System.Drawing.Point(23, 76);
             flpTabList.Name = "flpTabList";
-            flpTabList.Size = new System.Drawing.Size(436, 646);
+            flpTabList.Size = new System.Drawing.Size(284, 249);
             flpTabList.TabIndex = 0;
             flpTabList.WrapContents = false;
             flpTabList.DragDrop += flpTabList_DragDrop;
@@ -78,7 +78,7 @@ namespace AlligatorGh.Components.UI.PlugInManager
             flpMainLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
             flpMainLayout.RowStyles.Add(new RowStyle());
             flpMainLayout.RowStyles.Add(new RowStyle());
-            flpMainLayout.Size = new System.Drawing.Size(482, 894);
+            flpMainLayout.Size = new System.Drawing.Size(330, 461);
             flpMainLayout.TabIndex = 0;
             // 
             // tlpBottomPanel
@@ -90,13 +90,13 @@ namespace AlligatorGh.Components.UI.PlugInManager
             tlpBottomPanel.Controls.Add(btnReset, 0, 0);
             tlpBottomPanel.Controls.Add(btnSave, 1, 0);
             tlpBottomPanel.Dock = DockStyle.Fill;
-            tlpBottomPanel.Location = new System.Drawing.Point(20, 833);
+            tlpBottomPanel.Location = new System.Drawing.Point(20, 408);
             tlpBottomPanel.Margin = new Padding(0, 10, 0, 0);
             tlpBottomPanel.Name = "tlpBottomPanel";
             tlpBottomPanel.RowCount = 1;
             tlpBottomPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tlpBottomPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tlpBottomPanel.Size = new System.Drawing.Size(442, 41);
+            tlpBottomPanel.Size = new System.Drawing.Size(290, 33);
             tlpBottomPanel.TabIndex = 2;
             // 
             // btnReset
@@ -104,7 +104,7 @@ namespace AlligatorGh.Components.UI.PlugInManager
             btnReset.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             btnReset.Location = new System.Drawing.Point(3, 3);
             btnReset.Name = "btnReset";
-            btnReset.Size = new System.Drawing.Size(144, 35);
+            btnReset.Size = new System.Drawing.Size(124, 27);
             btnReset.TabIndex = 2;
             btnReset.Text = "Reset";
             btnReset.UseVisualStyleBackColor = true;
@@ -113,9 +113,9 @@ namespace AlligatorGh.Components.UI.PlugInManager
             // btnSave
             // 
             btnSave.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            btnSave.Location = new System.Drawing.Point(295, 3);
+            btnSave.Location = new System.Drawing.Point(163, 3);
             btnSave.Name = "btnSave";
-            btnSave.Size = new System.Drawing.Size(144, 35);
+            btnSave.Size = new System.Drawing.Size(124, 27);
             btnSave.TabIndex = 1;
             btnSave.Text = "Apply";
             btnSave.UseVisualStyleBackColor = true;
@@ -125,10 +125,10 @@ namespace AlligatorGh.Components.UI.PlugInManager
             // 
             lblSummary.AutoSize = true;
             lblSummary.Dock = DockStyle.Fill;
-            lblSummary.Location = new System.Drawing.Point(20, 753);
+            lblSummary.Location = new System.Drawing.Point(20, 348);
             lblSummary.Margin = new Padding(0, 20, 0, 20);
             lblSummary.Name = "lblSummary";
-            lblSummary.Size = new System.Drawing.Size(442, 50);
+            lblSummary.Size = new System.Drawing.Size(290, 30);
             lblSummary.TabIndex = 3;
             lblSummary.Text = "Total Tabs : 51 Natives : 10 Installed Plugins : 41 Visible : 12 Hidden : 39";
             lblSummary.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -149,15 +149,15 @@ namespace AlligatorGh.Components.UI.PlugInManager
             tlpTopPanel.Name = "tlpTopPanel";
             tlpTopPanel.RowCount = 1;
             tlpTopPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tlpTopPanel.Size = new System.Drawing.Size(442, 41);
+            tlpTopPanel.Size = new System.Drawing.Size(290, 33);
             tlpTopPanel.TabIndex = 4;
             // 
             // btnNone
             // 
             btnNone.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            btnNone.Location = new System.Drawing.Point(153, 3);
+            btnNone.Location = new System.Drawing.Point(133, 3);
             btnNone.Name = "btnNone";
-            btnNone.Size = new System.Drawing.Size(144, 35);
+            btnNone.Size = new System.Drawing.Size(124, 27);
             btnNone.TabIndex = 1;
             btnNone.Text = "Check None";
             btnNone.UseVisualStyleBackColor = true;
@@ -168,7 +168,7 @@ namespace AlligatorGh.Components.UI.PlugInManager
             btnAll.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             btnAll.Location = new System.Drawing.Point(3, 3);
             btnAll.Name = "btnAll";
-            btnAll.Size = new System.Drawing.Size(144, 35);
+            btnAll.Size = new System.Drawing.Size(124, 27);
             btnAll.TabIndex = 0;
             btnAll.Text = "Check All";
             btnAll.UseVisualStyleBackColor = true;
@@ -180,9 +180,9 @@ namespace AlligatorGh.Components.UI.PlugInManager
             chbIcon.AutoSize = true;
             chbIcon.Checked = true;
             chbIcon.CheckState = CheckState.Checked;
-            chbIcon.Location = new System.Drawing.Point(315, 9);
+            chbIcon.Location = new System.Drawing.Point(263, 11);
             chbIcon.Name = "chbIcon";
-            chbIcon.Size = new System.Drawing.Size(124, 29);
+            chbIcon.Size = new System.Drawing.Size(24, 19);
             chbIcon.TabIndex = 2;
             chbIcon.Text = "ShowIcons";
             chbIcon.UseVisualStyleBackColor = true;
@@ -192,7 +192,7 @@ namespace AlligatorGh.Components.UI.PlugInManager
             // 
             AutoScaleMode = AutoScaleMode.Inherit;
             BackColor = System.Drawing.Color.WhiteSmoke;
-            ClientSize = new System.Drawing.Size(482, 894);
+            ClientSize = new System.Drawing.Size(330, 461);
             Controls.Add(flpMainLayout);
             FormBorderStyle = FormBorderStyle.SizableToolWindow;
             MaximizeBox = false;
--- AlligatorGh/Components/UI/PlugInManager/PluginManagerFrm.cs
+++ AlligatorGh/Components/UI/PlugInManager/PluginManagerFrm.cs
@@ -24,6 +24,7 @@ namespace AlligatorGh.Components.UI.PlugInManager
         public PluginManagerFrm()
         {
             InitializeComponent();
+            this.AutoScaleMode = AutoScaleMode.Dpi;
             LoadData();
 
 
